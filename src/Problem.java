aaf7e5c504608983ed554327b2ab2fdc5729c437
Auberon López <auberonedu@gmail.com>
2025-03-31 15:43:49 -0700

    Initial skeleton

---
+++ src/Problem.java
@@ -0,0 +1,17 @@
+public class Problem {
+    /**
+     * Returns how many salamanders less than 3 years old will be at risk from a
+     * serious infection if a given salamander is infected.
+     * 
+     * When a salamander is infected, it will pass the disease along to all of
+     * its contacts. Each of those contacts will pass it on to their contacts
+     * and so on. Salamanders of any age can catch and spread the disease, but
+     * it is only a serious risk for salamanders strictly less than 3 years old.
+     * 
+     * @param initialPatient the first salamander to be infected
+     * @return the number of salamanders less than 3 years old that may be infected
+     */
+    public static int countSeriousInfections(Salamander initialPatient) {
+        return -1;
+    }
+}
---
+++ src/Salamander.java
@@ -0,0 +1,32 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Salamander {
+    private int age;
+    private Set<Salamander> contacts;
+
+    public int getAge() {
+        return age;
+    }
+
+    public Set<Salamander> getContacts() {
+        return new HashSet<>(contacts);
+    }
+
+    public void addContact(Salamander other) {
+        contacts.add(other);
+    }
+
+    public void removeContact(Salamander other) {
+        contacts.remove(other);
+    }
+
+    public Salamander(int age, Set<Salamander> contacts) {
+        this.age = age;
+        this.contacts = contacts;
+    }
+
+    public Salamander(int age) {
+        this(age, new HashSet<>());
+    }
+}
